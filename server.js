da68781a5a4914a3dcaf322fd3b949794ac4b7b9
Akeem <realowded@gmail.com>
2019-09-06 12:51:55 +0100

    Added projects page.

--- server.js
+++ server.js
@@ -61,6 +61,12 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
+
+app.get('/projects', (req, res)=> {
+    res.render('projects', {
+        pageTitle: 'Projects'
+    })
+})
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 
